4026578faea7725d7af3ccbf78f3c59f43147052
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2018-08-17 15:39:19 +0200

    comment added

--- src/iAQcore.cpp
+++ src/iAQcore.cpp
@@ -39,9 +39,9 @@ void iAQcore::read(uint16_t * eco2, uint16_t * stat, uint32_t * resist, uint16_t
   // Get bytes into local buf
   uint8_t buf[IAQCORE_SIZE];
   for( int i=0; i<IAQCORE_SIZE; i++ ) buf[i]= Wire.read();
-  // Pack into output parameters (little endian)
+  // Pack into output parameters (little-endian)
   if( eco2  !=0 ) *eco2  =  (buf[0]<<8) + (buf[1]<<0);
-  if( stat  !=0 ) *stat  =  ( num==IAQCORE_SIZE ? 0 : IAQCORE_STAT_I2CERR ) + (buf[2]<<0);
+  if( stat  !=0 ) *stat  =  ( num==IAQCORE_SIZE ? 0 : IAQCORE_STAT_I2CERR ) + (buf[2]<<0); // Add I2C status to chip status
   if( resist!=0 ) *resist=  ((uint32_t)buf[3]<<24) + ((uint32_t)buf[4]<<16) + ((uint32_t)buf[5]<<8) + ((uint32_t)buf[6]<<0); 
   if( etvoc !=0 ) *etvoc =  (buf[7]<<8) + (buf[8]<<0);
 }
